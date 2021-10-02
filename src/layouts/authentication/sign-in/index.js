74f469a985be0911d482ba64cf3c7722f70c37b7
jaydxn1 <NIL>
2021-10-03 00:54:11 +0800

    fixed sign-in submodule issue

---
+++ src/layouts/authentication/sign-in/index.js
@@ -0,0 +1,60 @@
+/**
+=========================================================
+* Soft UI Dashboard React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-material-ui
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { Fragment } from "react";
+
+import Amplify from "aws-amplify";
+import {
+  AmplifyAuthenticator,
+  AmplifySignOut,
+  AmplifySignIn,
+  AmplifySignUp,
+} from "@aws-amplify/ui-react";
+
+// Authentication layout components
+import CoverLayout from "layouts/authentication/components/CoverLayout";
+import SuiBox from "components/SuiBox";
+
+// Images
+import curved9 from "assets/images/sign-in.jpg";
+import awsconfig from "./aws-exports";
+
+import "./index.css";
+
+Amplify.configure(awsconfig);
+
+function SignIn() {
+  return (
+    <Fragment key="key">
+      <CoverLayout
+        title="Welcome to Kaizen"
+        description="Leading Digital Revolution in Construction."
+        image={curved9}
+      >
+        <SuiBox>
+          <AmplifyAuthenticator>
+            <AmplifySignIn headerText="Sign In" slot="sign-in" />
+            <AmplifySignUp headerText="Getting Started" slot="sign-up" />
+            <div>
+              <AmplifySignOut />
+            </div>
+          </AmplifyAuthenticator>
+        </SuiBox>
+      </CoverLayout>
+    </Fragment>
+  );
+}
+
+export default SignIn;
