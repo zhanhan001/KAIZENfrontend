deaf8e91e5d1e692b7dea85ecd701868f0689ee5
jaydxn1 <NIL>
2021-10-12 15:57:34 +0800

    styled stripe integration

--- src/layouts/labour-details/components/Checkout/index.js
+++ src/layouts/labour-details/components/Checkout/index.js
@@ -3,79 +3,52 @@ import SuiButton from "components/SuiButton";
 import Stripe from "react-stripe-checkout";
 import { Auth } from "aws-amplify";
 
-
-
-const ProductDisplay = () => (
-  <section>
-    <form action="/create-checkout-session" method="POST">
-      <SuiButton
-        size="large"
-        variant="gradient"
-        buttonColor="success"
-        type="submit"
-      >
-        Checkout
-      </SuiButton>
-    </form>
-  </section>
-);
-
-const Message = ({ message }) => (
-  <section>
-    <p>{message}</p>
-  </section>
-);
+import profileimage from "assets/images/team-1.jpg";
+import SuiBox from "components/SuiBox";
 
 export default function App() {
 
   async function handleToken(token) {
     console.log(token);
     Auth.currentSession().then((res) => {
-        fetch(`/api/payment/charge`, {
-          method: "POST",
-          headers: {
-            'Authorization': 'Bearer ' + res.getIdToken().getJwtToken(),
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'token' : token.id,
-            'amount' : '500',
+      fetch(`/api/payment/charge`, {
+        method: "POST",
+        headers: {
+          'Authorization': 'Bearer ' + res.getIdToken().getJwtToken(),
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'token': token.id,
+          'amount': '500',
 
-          },
-        }).then(() => {
-            alert("Payment Success");
-        })
+        },
+      }).then(() => {
+        alert("Payment Success \nYou will be contacted in 1-3 business days by the parent company regarding the hire-sharing.")
+      })
         .catch((error) => {
           alert(error);
         });
-      });
-    };
+    });
+  };
 
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    // Check to see if this is a redirect back from Checkout
-    const query = new URLSearchParams(window.location.search);
-
-    if (query.get("success")) {
-      setMessage("Order placed! You will receive an email confirmation.");
-    }
-
-    if (query.get("canceled")) {
-      setMessage(
-        "Order canceled -- continue to shop around and checkout when you're ready."
-      );
-    }
-  }, []);
-
-  return message ? (
-    <Message message={message} />
-  ) : (
-    <div>
-      <Stripe
-        stripeKey="pk_test_51JjP7qFwG6YcxwhyrbB3ljilisjDA3b28136bJ1pQONRGxQY5IxfddiZk3Wx69w8w60BEkIaOi3hVBTsB01yrq04004bU9S3XQ"
-        token={handleToken}
-      />
-      <ProductDisplay />
-    </div>
+  return (
+    <Stripe
+      name="James Smith" // the pop-in header title
+      description="Wan Kah Construction Pte Ltd" // the pop-in header subtitle
+      image={profileimage}
+      ComponentClass="div"
+      label="Submit" // text inside the Stripe button
+      panelLabel="Hire for" // prepended to the amount in the bottom pay button
+      currency="SGD"
+      amount={80000}
+      locale="en"
+      stripeKey="pk_test_51JjP7qFwG6YcxwhyrbB3ljilisjDA3b28136bJ1pQONRGxQY5IxfddiZk3Wx69w8w60BEkIaOi3hVBTsB01yrq04004bU9S3XQ"
+      token={handleToken}
+    >
+      <SuiButton size="large" variant="gradient" buttonColor="success" circular type="submit">
+        <SuiBox px={3} color="white">
+          Submit
+        </SuiBox>
+      </SuiButton>
+    </Stripe>
   );
 }
