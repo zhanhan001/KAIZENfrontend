854f6c54bc19b334c96cb435881a218dfffbe0da
BryanTZY <62585699+BryanTZY@users.noreply.github.com>
2021-10-15 17:19:40 +0800

    cleaned up generalnewscard code

--- src/layouts/dashboard/components/GeneralNewsCard/index.js
+++ src/layouts/dashboard/components/GeneralNewsCard/index.js
@@ -1,17 +1,3 @@
-/**
-=========================================================
-* Soft UI Dashboard React - v2.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-material-ui
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 
 // @mui material components
 import Card from "@mui/material/Card";
@@ -21,8 +7,6 @@ import Icon from "@mui/material/Icon";
 // Soft UI Dashboard React components
 import SuiBox from "components/SuiBox";
 import SuiTypography from "components/SuiTypography";
-import SuiAvatar from "components/SuiAvatar";
-
 
 // Custom styles for the BuildByDevelopers
 import styles from "layouts/dashboard/components/GeneralNewsCard/styles";
@@ -30,8 +14,6 @@ import { Auth } from 'aws-amplify';
 import { useState, useEffect } from "react";
 import Carousel from 'react-material-ui-carousel';
 
-// Images
-import mock from "assets/images/mock.svg";
 
 function GeneralNewsCard() {
   const classes = styles();
@@ -48,39 +30,6 @@ function GeneralNewsCard() {
     })
   }, []);
 
-  // let headers = boxes.map(box =>
-  //   <SuiTypography textColor="white">
-  //     <SuiTypography key={box.id} display="flex" variant="h5" fontWeight="bold" gutterBottom>
-  //       {box.header}
-  //     </SuiTypography>
-  //     <SuiBox
-
-  //             justifyContent="center"
-  //             alignItems="center"
-  //             borderRadius="50"
-  //             height="auto" p={3}
-  //             inputProps={{style:{height:50, width: 50}}}
-  //           ><img src={box.imageUrl} max-width="100%" class="img-fluid"/></SuiBox>
-  //     <SuiTypography key={box.id} display="flex" variant="body2" textColor="text">
-  //       {box.excerpt}
-  //     </SuiTypography>
-  //     {/* <SuiTypography key={box.id} style= {{height:70}} variant="h5" fontWeight="bold" gutterBottom>
-  //       {box}
-  //     </SuiTypography> */}
-
-  //     <SuiTypography key={box.id}
-  //       component="a"
-  //       href={box.url}
-  //       variant="button"
-  //       textColor="text"
-  //       fontWeight="medium"
-  //       customClass={classes.generalNewsCard_button}
-  //     > Read More
-  //       <Icon className="font-bold">arrow_forward</Icon>
-  //     </SuiTypography>
-
-  //   </SuiTypography>
-  // );
   let headers = boxes.map(box =>
     <Grid container spacing={3}>
       <Grid item item xs={12} lg={7}>
@@ -123,8 +72,6 @@ function GeneralNewsCard() {
       </Grid>
     </Grid>
   );
-
-
 
   return (
     <Card style={{height: "40vh"}}>
