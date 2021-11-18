c4fe4f8cc4e4d7fa18fbc8b58f9981ef767d6e08
zhanhan <62456706+zhanhan001@users.noreply.github.com>
2021-11-18 13:58:08 +0800

    Update chat.js

--- src/chat.js
+++ src/chat.js
@@ -18,7 +18,7 @@ class KommunicateChat extends Component{
     componentDidMount(){
 
         (function(d, m){
-            var kommunicateSettings = {"appId":"1326488dc77f91039518354a26d80239f","popupWidget":true,"automaticChatOpenOnNavigation":true};
+            var kommunicateSettings = {"appId":APP_ID,"popupWidget":true,"automaticChatOpenOnNavigation":true};
             var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
             s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
             var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
